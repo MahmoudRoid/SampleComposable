e27a29d4bd8bbfd87797cbdf334e3c6fa9fbc0e0
Mahmoudroid <m.mahmoudreza1@gmail.com>
2021-01-20 02:38:10 +0330

    domain model

---
+++ app/src/main/java/ir/mahmoudroid/samplecomposable/domain/model/Recipe.kt
@@ -0,0 +1,19 @@
+package ir.mahmoudroid.samplecomposable.domain.model
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Recipe (
+        val id: Int? = null,
+        val title: String? = null,
+        val publisher: String? = null,
+        val featuredImage: String? = null,
+        val rating: Int? = 0,
+        val sourceUrl: String? = null,
+        val description: String? = null,
+        val cookingInstructions: String? = null,
+        val ingredients: List<String> = listOf(),
+        val dateAdded: String? = null,
+        val dateUpdated: String? = null,
+) : Parcelable
